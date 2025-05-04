73bd2f3a72cef6839c7f41f880c69f1310de64a4
Angela Rosana Garcia <100092195+angelargd8@users.noreply.github.com>
2025-05-04 00:08:35 -0600

    feat: add CreateAccount

--- login.py
+++ login.py
@@ -29,3 +29,34 @@ def login(usuarios, correo, contrasena):
         messagebox.showerror("Error", "Usuario no encontrado")
         
 
+def CreateAccount(usuarios, nombre, email, constrasena, tipoCuenta=None):
+    usuario_id = ObjectId()
+    #buscar que el email no exista
+    val_email = usuarios.find_one({"email": email})
+
+    if nombre and email and constrasena:
+        if not val_email:
+            # Crear un nuevo usuario
+            nuevo_usuario = {
+                "_id": usuario_id,
+                "nombre": nombre,
+                "email": email,
+                "contrasena": constrasena,
+                "historialPedidos": [], 
+                "tipoCuenta": tipoCuenta,   
+            }
+            
+            # Insertar nuevo usuario en la base de datos
+            usuarios.insert_one(nuevo_usuario)
+            messagebox.showinfo("Crear cuenta", "Usuario creado con éxito")
+        else:
+            messagebox.showerror("Error", "El correo ya existe")
+
+    else:
+        messagebox.showerror("Error", "Por favor complete todos los campos")
+        return None
+
+    return usuario_id
+
+def CerrarSesion():
+    pass
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import ast
 from tkinter import *
 from tkinter import ttk 
 from tkinter import messagebox
-from login import login
+from login import login, CreateAccount
 
-uri = ""
+uri = "mongodb+srv://angel:angel123@cluster0.krfnzqa.mongodb.net"
 client = MongoClient(uri)
 db = client["restaurante"]
 print("Conectado a la base de datos")
@@ -23,6 +23,58 @@ ordenes = db["ordenes"]
 usuarios = db["usuarios"]
 
 global tipoCuenta
+global tabs
+tabs =[]
+
+
+def CerrarSesion():
+    global tabs
+    for tab_id in tab.tabs(): 
+        tab.forget(tab_id)
+    
+    tabs.clear()
+    # tab.destroy()
+    landingPage()
+
+def landingPage(tipoCuenta=None):
+    tabLanging = ttk.Frame(tab)
+    tab.add(tabLanging, text="Crear cuenta")
+    tabs.append(tabLanging)
+
+    l4 = Label(tabLanging, text="Ingrese su nombre:", fg="#6c584c", font=("Arial", 12))
+    l4.place(x=10, y=20)
+    e4 = Entry(tabLanging, width=30, font=("Arial", 12), bg="#f4f1e6")
+    e4.place(x=190, y=20)
+
+    l5 = Label(tabLanging, text="Ingrese su correo:", fg="#6c584c", font=("Arial", 12))
+    l5.place(x=10, y=50)
+    e5 = Entry(tabLanging, width=30, font=("Arial", 12), bg="#f4f1e6")
+    e5.place(x=190, y=50)
+
+    l6 = Label(tabLanging, text="Ingrese su contraseña:", fg="#6c584c", font=("Arial", 12))
+    l6.place(x=10, y=80)
+    e6 = Entry(tabLanging, width=30, font=("Arial", 12), bg="#f4f1e6")
+    e6.place(x=190, y=80)
+
+
+    # nombre = e4.get()
+    # email = e5.get()
+    # constrasena = e6.get()
+    # tipoCuenta = None
+
+    if tipoCuenta == "administrador":
+        l7 = Label(tabLanging, text="Ingrese el tipo de cuenta:", fg="#6c584c", font=("Arial", 12))
+        l7.place(x=10, y=110)
+        e7 = Entry(tabLanging, width=30, font=("Arial", 12), bg="#f4f1e6")
+        e7.place(x=190, y=110)
+
+        tipoCuenta = e7.get()
+
+
+    btn3 = Button(tabLanging, text="Crear cuenta", fg="#ffffff", font=("Arial", 12), bg="#78290f", 
+              command=lambda: CreateAccount(usuarios, e4.get(), e5.get(), e6.get(), tipoCuenta))
+    btn3.place(x=510, y=20, height=50)
+    return tabLanging
 
 def iniciarSesion(): 
     correo = e2.get()
@@ -32,32 +84,46 @@ def iniciarSesion():
     else:
         #validar el correo y la contraseña
         tipoCuenta = login(usuarios, correo, contrasena)
+
+        administrador = ["agregar/eliminar restaurante", "agregar/eliminar producto", "agregar/eliminar/editar menu", "agregar/eliminar usuarios de consultores"]
+        consultor = ["obtener estadisticas", "reporte de resenias con ordenamiento arbitrario"]
+        cliente = ["crear/eliminar cuenta", "editar informacion de la cuenta", "realizar una resena de un producto especifico", "realizar una resena de un combo"]
+        mesero = ["crear orden", "ver orden", "cambiar el estado de la orden"]
+
+        
         
         if tipoCuenta == "administrador":
+            global tabs
+            for tab_id in tabs: 
+                tab.forget(tab_id)
+                
+            tabLanging = landingPage(tipoCuenta)
+            tabs.append(tabLanging)
             
-            tab1= ttk.Frame(tab)
-            tab.add(tab1, text="agregar/eliminar restaurante")
-            tab2= ttk.Frame(tab)
-            tab.add(tab2, text="agregar/eliminar producto")
-            tab3= ttk.Frame(tab)
-            tab.add(tab3, text="agregar/eliminar/editar menu")
-            tab4= ttk.Frame(tab)
-            tab.add(tab4, text="agregar/eliminar usuarios de consultores")
+            for i in range(len(administrador)): 
+                frame = ttk.Frame(tab)
+                tab.add(frame, text=administrador[i])
+                tabs.append(frame)
+            
+                
 
         elif tipoCuenta == "consultor":
-            pass
-        elif tipoCuenta == "cliente":
-            pass
-        elif tipoCuenta == "mesero":
-            tab1= ttk.Frame(tab)
-            tab.add(tab1, text="ingreso de orden")
-            tab2= ttk.Frame(tab)
-            tab.add(tab2, text="consulta general")
-            tab3= ttk.Frame(tab)
-            tab.add(tab3, text="consulta individual")
-            tab4= ttk.Frame(tab)
-            tab.add(tab4, text="modificacion de datos")
+            for i in consultor: 
+                frame = ttk.Frame(tab)
+                tab.add(frame, text=consultor[i])
+                tabs.append(frame)
 
+        elif tipoCuenta == "cliente":
+            for i in cliente: 
+                frame = ttk.Frame(tab)
+                tab.add(frame, text=cliente[i])
+                tabs.append(frame)
+
+        elif tipoCuenta == "mesero":
+            for i in mesero: 
+                frame = ttk.Frame(tab)
+                tab.add(frame, text=mesero[i])
+                tabs.append(frame)
 
 v = Tk()
 v.title("caféCITo ☕🍪")
@@ -69,15 +135,18 @@ l2 = Label(v, text="Ingrese su correo:", fg= "#6c584c" , font=("Arial", 12),bg="
 l2.place(x=10, y=80)
 e2 = Entry(v, width=30, font=("Arial", 12), bg="#f4f1e6")
 e2.place(x=190, y=80)
+e2.insert(0, "hsolomon@example.org")
 
 l3 = Label(v, text="Ingrese su contraseña:", fg= "#6c584c" , font=("Arial", 12),bg="#e3d5ca")
 l3.place(x=10, y=110)
 e3 = Entry(v, width=30, font=("Arial", 12), bg="#f4f1e6")
 e3.place(x=190, y=110)
+e3.insert(0, "!*5OuIQsxg")
 
 btn1 = Button(v, text="Iniciar sesión", fg="#6c584c", font=("Arial", 12), bg="#f4f1e6", command=iniciarSesion)
 btn1.place(x=510, y=80, height=50)
-
+btn2 = Button(v, text="Cerrar sesión", fg="#ffffff", font=("Arial", 12), bg="#78290f", command=CerrarSesion)
+btn2.place(x=630, y=80, height=50)
 
 #pestañas 
 tab= ttk.Notebook(v)
@@ -85,6 +154,7 @@ tab.pack()
 tab.config(width="800", height="500")
 tab.place(x=10,y=150)
 
+landingPage()
 
 
 # v.resizable(0, 0)
