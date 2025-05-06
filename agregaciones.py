526639fb4df061a98cfb15bae8fd8c7dcf050060
Angela Rosana Garcia <100092195+angelargd8@users.noreply.github.com>
2025-05-06 14:29:45 -0600

    Create agregaciones.py

---
+++ agregaciones.py
@@ -0,0 +1,6 @@
+#agregaciones
+
+# - simples (count, distinct, sum, avg, min, max)
+# - complejas (group, project, unwind, match, sort)
+# - manejo de arrays (push, addToSet, $slice, $elemMatch)
+# - manejo de documentos embebidos (lookup, unwind, group, project)
